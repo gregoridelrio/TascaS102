7a203864ddfa9b65f1ee7f4c9e79957784365fe0
Laurea Gregori <gregoridelrio@gmail.com>
2025-12-10 11:31:27 +0100

    feat: added echo

--- nivell1/exercici6.php
+++ nivell1/exercici6.php
@@ -1,9 +1,13 @@
 <?php
-  function isBitten(){
-    $bite = rand(1,2);
-    if ($bite == 1){
-      return true;
-    } else {
-      return false;
-    }
+function isBitten()
+{
+  $bite = rand(1, 2);
+  if ($bite == 1) {
+    return true;
+  } else {
+    return false;
   }
+}
+
+// Añadido echo para mostrar resultado de la funcion
+echo $status = (isBitten()) ? "Charlie bit your finger" : "Charlie hasn't bitten your finger";
